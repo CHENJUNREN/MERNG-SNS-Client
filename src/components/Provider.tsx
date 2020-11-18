e718bdde9c8dcfc3fb81cd07419bfbbbbc9d4d99
Chenjun <tonyren@my.yorku.ca>
2020-11-18 01:55:48 -0500

    edited proxy and serve base URL

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -9,7 +9,7 @@ import React from "react";
 import { setContext } from "@apollo/link-context";
 
 const httpLink = createHttpLink({
-	uri: "http://localhost:5000",
+	uri: "https://morning-refuge-92909.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
